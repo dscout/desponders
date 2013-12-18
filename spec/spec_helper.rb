6f31efdc33015585e9a2c3997d9289f06b3fe972
Parker Selbert <parker@sorentwo.com>
2013-12-17 21:05:46 -0600

    Prevent I18n deprecation warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,9 @@ ENV['RAILS_ENV'] = 'test'
 
 require 'desponders'
 
+# Prevent deprecation warning
+I18n.enforce_available_locales = false
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
